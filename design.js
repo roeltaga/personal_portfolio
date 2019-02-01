2140cd4d18af82856f81bb2bb8f65dd7a46cfd50
Roel Taga <roeltaga@gmail.com>
2019-02-02 00:29:43 +0100

    JS update

--- design.js
+++ design.js
@@ -1,6 +1,30 @@
 window.onload = function() {
 
 
+
+function readCookies() {     /* READ COOKEIS */
+    cookiesString = document.cookie.split('; ');
+    var result = {};
+    for (var i = 0; i < cookiesString.length; i++) {
+        var cur = cookiesString[i].split('=');
+        result[cur[0]] = cur[1];
+    }
+    cookies = result;
+}
+readCookies();
+console.log(cookies.filterLogo);
+console.log(document.cookie);
+
+if (cookies.filterLogo == 0) {
+    document.getElementById("filterLogo").classList.remove("filterIsOn")
+    document.getElementById("filterLogo").classList.add("filterIsOff")
+}
+
+
+
+
+
+
 let filters = document.getElementById("filters_button");
 
 function roel() {
@@ -17,6 +41,10 @@ let anyfilter = this.document.getElementsByClassName("filterFilter");
 function changeState(i) {
     this.classList.toggle("filterIsOn");
     this.classList.toggle("filterIsOff");
+    if (this.classList.contains("filterIsOn")) {
+        document.cookie = "filterLogo=1; expires=Thu, 14 Feb 2019 20:47:11 UTC; path=/"
+    }
+    else { document.cookie = "filterLogo=0; expires=Thu, 14 Feb 2019 20:47:11 UTC; path=/"; }
 }
 
 for (i=0; i < anyfilter.length; i++) {
